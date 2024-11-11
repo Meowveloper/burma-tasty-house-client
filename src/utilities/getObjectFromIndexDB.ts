ee0e29c7a503123d9f39e6f2c46aaf8ff8117f42
Meowveloper <meowveloper.dev@gmail.com>
2024-11-11 22:42:27 +0700

    fix indexdb error

--- src/utilities/getObjectFromIndexDB.ts
+++ src/utilities/getObjectFromIndexDB.ts
@@ -1,28 +1,37 @@
 import IRecipe from "../types/IRecipe";
 
 function getObjectFromIndexedDB(callback: (data: IRecipe) => void) {
-  const request = indexedDB.open("recipeDB", 1);
+    const request = indexedDB.open("recipeDB", 1);
+    request.onupgradeneeded = () => {
+        const db = request.result;
 
-  request.onsuccess = function (event) {
-    const db = (event.target as IDBOpenDBRequest).result;
-    const transaction = db.transaction("recipes", "readonly");
-    const store = transaction.objectStore("recipes");
-
-    const objectRequest = store.get("recipe"); // Use the same key you stored with
-
-    objectRequest.onsuccess = function () {
-      const data = objectRequest.result;
-      callback(data); // This should now return the correct object
+        // Create the object store if it doesn't exist
+        if (!db.objectStoreNames.contains("recipes")) {
+            db.createObjectStore("recipes", { keyPath: "key" });
+            console.log("Object store 'recipes' created");
+        }
     };
 
-    objectRequest.onerror = function (event) {
-      console.error("Error retrieving object:", event);
-    };
-  };
+    request.onsuccess = function (event) {
+        const db = (event.target as IDBOpenDBRequest).result;
+        const transaction = db.transaction("recipes", "readonly");
+        const store = transaction.objectStore("recipes");
 
-  request.onerror = function (event) {
-    console.error("Error opening IndexedDB:", event);
-  };
+        const objectRequest = store.get("recipe"); // Use the same key you stored with
+
+        objectRequest.onsuccess = function () {
+            const data = objectRequest.result;
+            callback(data); // This should now return the correct object
+        };
+
+        objectRequest.onerror = function (event) {
+            console.error("Error retrieving object:", event);
+        };
+    };
+
+    request.onerror = function (event) {
+        console.error("Error opening IndexedDB:", event);
+    };
 }
 
 export default getObjectFromIndexedDB;
