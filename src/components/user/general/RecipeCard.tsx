f649961c2912d2a43c498b557f2462eb00c2046a
Meowveloper <meowveloper.dev@gmail.com>
2024-11-14 20:53:34 +0700

    profile and recipe detail page

--- src/components/user/general/RecipeCard.tsx
+++ src/components/user/general/RecipeCard.tsx
@@ -1,16 +1,20 @@
 import IRecipe from "../../../types/IRecipe";
 
-interface IProps {
-    recipe: IRecipe;
-}
 
-export default function RecipeCard(props: IProps) {
-    return (
-        <div className="dark:bg-dark-card p-4 rounded-small">
-            <div className="text-h3 font-bold dark:text-dark-text-highlight">{ props.recipe.title }</div>
-            <img src={props.recipe.image as string} className="w-full h-[150px] rounded-small" />
-            <div>duration - {props.recipe.preparation_time}</div>
-            <div>difficulty level - {props.recipe.difficulty_level}</div>
-        </div>
-    );
+export default function RecipeCard(recipe : IRecipe) {
+    return function (setRecipeToShow: React.Dispatch<React.SetStateAction<IRecipe | null>>) {
+        return (
+            <div
+                className="dark:bg-dark-card p-4 rounded-small"
+                onClick={() => {
+                    setRecipeToShow(recipe);
+                }}
+            >
+                <div className="text-h3 font-bold dark:text-dark-text-highlight">{recipe.title}</div>
+                <img src={recipe.image as string} className="w-full h-[150px] rounded-small" />
+                <div>duration - {recipe.preparation_time}</div>
+                <div>difficulty level - {recipe.difficulty_level}</div>
+            </div>
+        );
+    };
 }
--- src/pages/user/Profile.tsx
+++ src/pages/user/Profile.tsx
@@ -1,16 +1,18 @@
-import { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
 import { useParams } from "react-router-dom";
 import axios from "../../utilities/axios";
 import IUser from "../../types/IUser";
 import IRecipe from "../../types/IRecipe";
 import RecipeCard from "../../components/user/general/RecipeCard";
-export default function Profile() {
+import Preview from "../../components/user/RecipeForm/Preview";
+export default function Profile() : JSX.Element {
     const authContext = useContext(AuthContext);
     const params = useParams();
     const userId = params.id;
     const [user, setUser] = useState<IUser>({} as IUser);
     const [loading, setLoading] = useState<boolean>(false);
+    const [ recipeToShow , setRecipeToShow ] = useState<IRecipe | null>(null);
 
     useEffect(() => {
         setLoading(true);
@@ -32,6 +34,15 @@ export default function Profile() {
 
     if (loading) return <div>Loading.....</div>;
 
+    if(recipeToShow) return (
+        <div>
+            { Preview(recipeToShow)(true)()()()(user) }
+            <button onClick={ () => { setRecipeToShow(null); } } className="dark:bg-dark-card px-4 py-2 cursor-pointer rounded-small">Back</button>
+        </div>
+    ); 
+    
+    
+
     return (
         <div>
             <div className="text-h1">
@@ -41,7 +52,7 @@ export default function Profile() {
 
             <div>
                 <div className="text-h1 font-bold">Recipes</div>
-                <div className="grid grid-cols-2 gap-4">{!!user.recipes?.length && (user.recipes as IRecipe[]).map((item: IRecipe) => <RecipeCard key={item._id} recipe={item}></RecipeCard>)}</div>
+                <div className="grid grid-cols-2 gap-4">{!!user.recipes?.length && (user.recipes as IRecipe[]).map((item: IRecipe) => <React.Fragment key={item._id}>{ RecipeCard(item)(setRecipeToShow) }</React.Fragment>)}</div>
             </div>
         </div>
     );
