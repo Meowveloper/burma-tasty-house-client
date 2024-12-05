6b4919b58c026f9997e88debf6839ce01d573026
Meowveloper <meowveloper.dev@gmail.com>
2024-12-05 18:34:50 +0700

    clean up home page

--- src/pages/user/Home.tsx
+++ src/pages/user/Home.tsx
@@ -1,13 +1,14 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useMemo, useState } from "react";
 import IRecipe from "../../types/IRecipe";
 import axios from "../../utilities/axios";
 import RecipeCard from "../../components/user/general/RecipeCard";
 import { AuthContext } from "../../contexts/AuthContext";
 import RecipeDetail from "./RecipeDetail";
-import {  deleteRecipeInBackendAndRemoveRecipeFromStates } from "../../utilities/generalHelperFunctions";
+import { deleteRecipeInBackendAndRemoveRecipeFromStates } from "../../utilities/generalHelperFunctions";
 enum EnumRoutesForFetchingRecipesWithLimits {
     latest = "/recipes/latest",
     highestView = "/recipes/highest-view",
+    highestComment = "/recipes/highest-comment"
 }
 export default function UserHome() {
     const authContext = useContext(AuthContext);
@@ -17,46 +18,21 @@ export default function UserHome() {
     const [highestViewRecipesLoading, setHighestViewRecipesLoading] = useState<boolean>(false);
     const [recipeToShow, setRecipeToShow] = useState<IRecipe | null>(null);
 
-    useEffect(() => {
-        console.log("checking infinite loop from pages/user/Home.tsx for latest recipes");
-        setLatestRecipesLoading(true);
-        const getLatestRecipesWithLimit = getRecipesWithLimit(EnumRoutesForFetchingRecipesWithLimits.latest);
-        getLatestRecipesWithLimit(5)
-            .then(res => {
-                setLatestRecipes(res);
-            })
-            .catch(e => {
-                console.log(e);
-            })
-            .finally(() => {
-                setLatestRecipesLoading(false);
-            });
-    }, []);
+    const limit: Readonly<number> = 5;
+
+    const addViewsToRecipes = useMemo(getAddViewsToRecipesFunction, [recipeToShow, authContext.user]);
 
     useEffect(() => {
-        console.log("checking infinite loop from pages/user/Home.tsx for highest view recipes");
-        setHighestViewRecipesLoading(true);
-        const getHighestViewRecipesWithLimit = getRecipesWithLimit(EnumRoutesForFetchingRecipesWithLimits.highestView);
-        getHighestViewRecipesWithLimit(5)
-            .then(res => {
-                setHighestViewRecipes(res);
-            })
-            .catch(e => {
-                console.log(e);
-            })
-            .finally(() => {
-                setHighestViewRecipesLoading(false);
-            });
-    }, []);
+        console.log("checking infinite loop from pages/user/Home.tsx");
+        fetchLatestRecipes(); // limit = 5
+        fetchHighestViewRecipes(); // limit = 5
 
-    useEffect(() => {
-        console.log("checking infinite loop from pages/user/Home.tsx for adding views to recipes");
-        if (recipeToShow && authContext.user && authContext.user._id !== (typeof recipeToShow.user === "string" ? recipeToShow.user : recipeToShow.user._id)) {
-            setLatestRecipes(prev => findInRecipeArrayAndAddOneView(prev, recipeToShow._id));
-            setHighestViewRecipes(prev => findInRecipeArrayAndAddOneView(prev, recipeToShow._id));
-            console.log("added view in client");
-        }
-    }, [recipeToShow, authContext.user]);
+
+        
+        addViewsToRecipes();
+    }, [addViewsToRecipes]);
+
+
 
     const deleteRecipeAndRemoveFromHighestAndLatestRecipes = deleteRecipeInBackendAndRemoveRecipeFromStates(recipeToShow?._id, [setLatestRecipes, setHighestViewRecipes], () => setRecipeToShow(null));
 
@@ -69,7 +45,7 @@ export default function UserHome() {
                 <div className="flex items-center gap-5">
                     <div className="text-h1">Latest Recipes</div>
                     <div>{`>>`}</div>
-                    <div>See All Recipes</div>
+                    <div className="cursor-pointer underline">See All Recipes</div>
                 </div>
                 <div className="overflow-x-auto overflow-y-hidden whitespace-nowrap">
                     <div className="flex flex-row items-center gap-5 p-4">
@@ -85,7 +61,7 @@ export default function UserHome() {
                 <div className="flex items-center gap-5">
                     <div className="text-h1">Popular by Views</div>
                     <div>{`>>`}</div>
-                    <div>See more</div>
+                    <div className="cursor-pointer underline">See more</div>
                 </div>
                 <div className="overflow-x-auto overflow-y-hidden whitespace-nowrap">
                     <div className="flex flex-row items-center gap-5 p-4">
@@ -95,11 +71,70 @@ export default function UserHome() {
                 </div>
             </div>
             {/* highest view recipes end */}
+
+
+            {/* popular by comments recipes */}
+            <div>
+                <div className="flex items-center gap-5">
+                    <div className="text-h1">Popular by Comments</div>
+                    <div>{`>>`}</div>
+                    <div className="cursor-pointer underline">See more</div>
+                </div>
+                <div className="overflow-x-auto overflow-y-hidden whitespace-nowrap">
+                    <div className="flex flex-row items-center gap-5 p-4">
+                        {highestViewRecipesLoading && <div>Loading.....</div>}
+                        {!!highestViewRecipes.length && !latestRecipesLoading && highestViewRecipes.map((item: IRecipe) => <React.Fragment key={item._id}>{RecipeCard(item, authContext)(setRecipeToShow)}</React.Fragment>)}
+                    </div>
+                </div>
+            </div>
+            {/* popular by comments recipes end */}
         </div>
     );
+
+    async function fetchLatestRecipes() {
+        setLatestRecipesLoading(true);
+        const getLatestRecipesWithLimit = getRecipesWithLimit(EnumRoutesForFetchingRecipesWithLimits.latest);
+
+        getLatestRecipesWithLimit(limit)
+            .then(res => {
+                setLatestRecipes(res);
+            })
+            .catch(e => {
+                console.log(e);
+            })
+            .finally(() => {
+                setLatestRecipesLoading(false);
+            });
+    }
+
+
+    async function fetchHighestViewRecipes() {
+        
+        setHighestViewRecipesLoading(true);
+        const getHighestViewRecipesWithLimit = getRecipesWithLimit(EnumRoutesForFetchingRecipesWithLimits.highestView);
+        getHighestViewRecipesWithLimit(limit)
+            .then(res => {
+                setHighestViewRecipes(res);
+            })
+            .catch(e => {
+                console.log(e);
+            })
+            .finally(() => {
+                setHighestViewRecipesLoading(false);
+            });
+    }
+
+
+    function getAddViewsToRecipesFunction () : () => void {
+        return function () {
+            if (recipeToShow && authContext.user && authContext.user._id !== (typeof recipeToShow.user === "string" ? recipeToShow.user : recipeToShow.user._id)) {
+                setLatestRecipes(prev => findInRecipeArrayAndAddOneView(prev, recipeToShow._id));
+                setHighestViewRecipes(prev => findInRecipeArrayAndAddOneView(prev, recipeToShow._id));
+                console.log("added view in client");
+            }
+        }
+    }
 }
-
-
 
 // outside
 function getRecipesWithLimit(url: EnumRoutesForFetchingRecipesWithLimits): (limit: number) => Promise<IRecipe[]> {
@@ -124,4 +159,3 @@ function findInRecipeArrayAndAddOneView(recipes: IRecipe[], _id: IRecipe["_id"])
     });
     return newRecipes;
 }
-
