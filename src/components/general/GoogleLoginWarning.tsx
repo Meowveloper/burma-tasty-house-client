200c1afb46e53d8c2c3b6c99278094ebfa7d95b0
Meowveloper <meowveloper.dev@gmail.com>
2025-01-10 23:34:18 +0700

    change text

--- src/components/general/GoogleLoginWarning.tsx
+++ src/components/general/GoogleLoginWarning.tsx
@@ -1,3 +1,3 @@
 export default function GoogleLoginWarning() {
-    return <div className="text-red-400 font-bold text-justify">If you used google login/register cannot change their password because we do NOT access or store password or any other sensitive information of your google account in our database. Your account will be safe as long as your google account is safe. </div>;
+    return <div className="text-red-400 font-bold text-justify">If you used google login/register, you cannot change your password because we do NOT access or store password or any other sensitive information of your google account in our database. Your account will be safe as long as your google account is safe. </div>;
 }
